0963cd9254365033c15531d819c9197b6b75610c
abdulhaseeb <haseebharoon147@gmail.com>
2023-06-03 10:20:32 +0500

    Work Cmplete

---
+++ App.js
@@ -0,0 +1,49 @@
+var hour = 0
+var min = 0
+var sec = 0
+var msec = 0
+var interval
+var hourheading = document.getElementById("hour")
+var minheading = document.getElementById("min")
+var secheading = document.getElementById("sec")
+var msecheading = document.getElementById("msec")
+
+function timer() {
+    msec++
+    msecheading.innerHTML = msec
+    if (msec == 100) {
+        sec++
+        secheading.innerHTML = sec
+        msec = 0
+    }
+    else if (sec == 60) {
+        min++
+        minheading.innerHTML = min
+        sec = 0
+    }
+    else if (min == 60) {
+        hour++
+        hourheading.innerHTML = hour
+        min = 0
+    }
+
+}
+function start() {
+    interval = setInterval(timer, 10)
+}
+function pause() {
+    clearInterval(interval)
+}
+function reset() {
+    hour = 0
+    min = 0
+    sec = 0
+    msec = 0
+    hourheading.innerHTML = hour
+    minheading.innerHTML = min
+    secheading.innerHTML = sec
+    msecheading.innerHTML = msec
+
+}
+
+
